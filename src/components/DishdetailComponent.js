58f885ef02d3d4fd61d7f69932730fc798719ec8
tmtran <tmtran@knox.edu>
2019-01-19 10:45:21 -0600

    Functional Compononent

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,23 +1,33 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
 
-class Dishdetail extends Component{
-	constructor(props) {
-		super(props);
+	function RenderDish({dish}) {
+		if(dish!=null){
+			return (
+				<div className="container">
+	        <div className="row">
+						<div key={dish.id} className="col-12 col-md-5 m-1">
+							<Card>
+								<CardImg width="100%" src={dish.image} alt={dish.name} />
+								<CardBody>
+									<CardTitle>{ dish.name }</CardTitle>
+									<CardText>{ dish.description }</CardText>
+								</CardBody>
+							</Card>
+						</div>
+					</div>
+				</div>
+			);
+		} else {
+			return null;
+		}
 	}
 
-	render() {
-		const dish = this.props.dish;
-		return (
-			<Card>
-				<CardImg width="100%" src={dish.image} alt={dish.name} />
-				<CardBody>
-					<CardTitle>{ dish.name }</CardTitle>
-					<CardText>{ dish.description }</CardText>
-				</CardBody>
-			</Card>
+	const DishDetail = (props) => {
+		return(
+			<RenderDish dish={props.dish}/>
 		);
 	}
-}
 
-export default Dishdetail
+
+export default DishDetail
