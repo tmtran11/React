2a005a3f6042be92beafa0cbe0aa09d097d74e44
tmtran <tmtran@knox.edu>
2019-01-19 23:26:51 -0600

    Single page application

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,10 +1,21 @@
 import React from 'react';
-import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
+import { Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
 	function RenderDish({dish}) {
 		if(dish!=null){
 			return (
 				<div className="container">
+					<div className="row">
+						<Breadcrumb>
+							<BreadcrumbItem><Link to='/menu'>Menu</Link></BreadcrumbItem>
+							<BreadcrumbItem active>{dish.name}</BreadcrumbItem>
+						</Breadcrumb>
+						<div className="col-12">
+							<h3>{dish.name}</h3>
+							<hr />
+						</div>
+					</div>
 	        <div className="row">
 						<div key={dish.id} className="col-12 col-md-5 m-1">
 							<Card>
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -2,9 +2,13 @@ import React, { Component } from 'react';
 import Home from './HomeComponent';
 import Header from './HeaderComponent';
 import Menu from './MenuComponent';
+import Contact from './ContactComponent';
 import DishDetail from './DishdetailComponent';
 import Footer from './FooterComponent';
 import { DISHES } from '../shared/dishes';
+import { COMMENTS } from '../shared/comments';
+import { LEADERS } from '../shared/leaders';
+import { PROMOTIONS } from '../shared/promotions';
 import { Switch, Route, Redirect } from 'react-router-dom';
 
 class Main extends Component {
@@ -12,14 +16,26 @@ class Main extends Component {
   constructor(props) {
     super(props);
     this.state = {
-        dishes: DISHES
+        dishes: DISHES,
+        comments: COMMENTS,
+        leaders:LEADERS,
+        promotions: PROMOTIONS
     };
   }
 
   render() {
+    const DishWithId = ({match}) => {
+      return(
+        <DishDetail dish={this.state.dishes.filter((dish) => dish.id === parseInt(match.params.dishId, 10))[0]}
+        comments={this.state.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId, 10))[0]}
+        />
+      );
+    };
     const HomePage = () => {
         return(
-          <Home />
+          <Home dish={this.state.dishes.filter((dish) => dish.featured)[0]}
+          promotion={this.state.promotions.filter((promotion) => promotion.featured)[0]}
+          leader={this.state.leaders.filter((leader) => leader.featured)[0]}/>
         );
     }
 
@@ -29,6 +45,8 @@ class Main extends Component {
         <Switch>
           <Route path="/home" component={HomePage}/>
           <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes}/>}/>
+          <Route path="/menu/:dishId" component={DishWithId}/>
+          <Route exact path="/contactus" component={Contact}/>
           <Redirect to="/home" />
         </Switch>
         <Footer/>
